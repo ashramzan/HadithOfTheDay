948517a1ef60254a7defe62d8a340e54f8fd8106
ashramzan <ashalramzan@yahoo.co.uk>
2023-12-12 23:52:56 +0000

    fixes

--- App.js
+++ App.js
@@ -1,11 +1,159 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons/faQuoteLeft'
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons/faQuoteRight'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import {  useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_300Light_Italic } from '@expo-google-fonts/roboto';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
+import { faVolumeUp } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import * as Speech from 'expo-speech';
+import * as Clipboard from 'expo-clipboard';
+
+library.add(fab, faQuoteLeft)
+
 
 export default function App() {
+  const [Quote, setQuote] = useState('Loading hadith...');
+  const [Author, setAuthor] = useState('Loading..');
+  const [Reference, setReference] = useState('Loading..');
+  const [isLoading, setIsLoading] = useState(false);
+  const [copiedText, setCopiedText] = useState('');
+
+  const speak = () => {
+    Speech.stop();
+    Speech.speak(Quote + Author);
+  };
+
+  const copyToClipboard = async () => {
+    const fullText = `${Quote}\n\n${Author}\n${Reference}`;
+    await Clipboard.setStringAsync(fullText);
+  };
+
+  const fetchCopiedText = async () => {
+    const text = await Clipboard.getStringAsync();
+    setCopiedText(text);
+  };
+
+  const apiURLs = [
+    'https://random-hadith-generator.vercel.app/bukhari/',
+    'https://random-hadith-generator.vercel.app/muslim/',
+    'https://random-hadith-generator.vercel.app/abudawud/',
+    'https://random-hadith-generator.vercel.app/ibnmajah/',
+    'https://random-hadith-generator.vercel.app/tirmidhi/'
+  ]
+
+const fetchHadith = (apiURL) => {
+  setIsLoading(true);
+  fetch(apiURL).then(res => res.json()).then(result => {
+    // console.log(result.data.hadith_english);
+    const trimmedQuote = result.data.hadith_english.trim();
+
+    setQuote(trimmedQuote);
+    setAuthor(result.data.header);
+    setReference(result.data.refno);
+    setIsLoading(false);
+  })
+}
+
+const randomQuote = () => {
+  const randomApiUrl = apiURLs[Math.floor(Math.random() * apiURLs.length)];
+  fetchHadith(randomApiUrl);
+  this.ScrollView.scrollTo({y: 0, animated: true});
+}
+
+useEffect(() => {
+  randomQuote();
+}, []);
+
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_300Light_Italic
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{
+        width:'90%',
+        backgroundColor:'#fff',
+        borderRadius:20,
+        padding:20,
+        height:500}}>
+            <StatusBar barStyle="dark-content"/>
+          <Text style={{
+            textAlign:'center',
+            fontSize:24,
+            fontWeight:'600',
+            fontFamily: 'Roboto_400Regular',
+            color:'#AF7AC5',
+            marginBottom:20,
+            letterSpacing: 5
+            }}>
+            HADITH OF THE DAY
+          </Text>
+          <ScrollView ref={(ref) => (this.ScrollView = ref)}>          
+            <FontAwesomeIcon icon={faQuoteLeft} color='#D0D3D4'  style={{marginLeft:'2%',marginBottom:-12}}/>
+            <Text style={{
+              color:'#AF7AC5',
+              fontSize:16,
+              lineHeight:26,
+              letterSpacing:1.1,
+              fontWeight:'400',
+              fontFamily: 'Roboto_500Medium', 
+              textAlign:'center',
+              paddingHorizontal:30, 
+              marginBottom:10}}>
+              {Quote}
+            </Text>
+            <FontAwesomeIcon icon={faQuoteRight}  color='#D0D3D4' style={{marginLeft:'92%',marginTop:-10, marginBottom:5}}/>
+            <Text style={{textAlign:'right',fontWeight:'300',fontFamily:'Roboto_300Light_Italic',fontSize:12,color:'#AF7AC5', marginBottom: 2}}>
+            {Author}
+            </Text>
+            <Text style={{textAlign:'right',fontWeight:'300',fontFamily:'Roboto_300Light_Italic',fontSize:12,color:'#AF7AC5'}}>
+            {Reference}
+            </Text>
+          </ScrollView>
+          <TouchableOpacity 
+          onPress={randomQuote}
+           style={{
+            backgroundColor: isLoading ? 'rgba(250, 219, 216, 0.5)' : 'rgba(250, 219, 216, 1)',
+            padding:20,
+            borderRadius:30,
+            marginVertical:20,
+            }}>
+              <Text style={{color:'#AF7AC5',textAlign:'center',fontFamily: 'Roboto_400Regular',fontSize:18}}>
+                {isLoading ? "Loading..." : "Next Hadith"}
+              </Text>
+            </TouchableOpacity>
+            <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
+              <TouchableOpacity
+              onPress={speak}
+              style={{
+                borderWidth: 2,
+                borderColor: '#FADBD8',
+                borderRadius: 50,
+                padding: 15
+              }}>
+                <FontAwesomeIcon icon={faVolumeUp} size = '20x' color='#FADBD8'/>
+              </TouchableOpacity>
+              <TouchableOpacity
+              onPress={copyToClipboard}
+              style={{
+                borderWidth: 2,
+                borderColor: '#FADBD8',
+                borderRadius: 50,
+                padding: 15
+              }}>
+                <FontAwesomeIcon icon={faCopy} size = '20x' color='#FADBD8'/>
+              </TouchableOpacity>
+            </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +161,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FADBD8',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    textAlign:'center',
+    fontSize:26,
+    fontWeight:'600',
+    fontFamily: 'Roboto_400Regular',
+    color:'#AF7AC5',
+    letterSpacing: 5,
+  }
 });
